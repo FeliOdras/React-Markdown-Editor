14aa67bf3a53c80bf7a8b564bb2b0445ebccbada
FeliOdras <ariane.feldmann@odras.de>
2019-04-04 11:37:16 +0200

    Entered Markup for editor

---
+++ src/components/MarkdownEditor.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class MarkdownEditor extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Input</h3>
+                <p>Enter some markdown</p>
+                <textarea className="markdown-input" cols="40" rows="8"></textarea>
+                <h3>Output</h3>
+                <div className="markdown-output"></div>
+            </div>
+        )
+    }
+}
+
+export default MarkdownEditor
